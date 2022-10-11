c016fc4577488fe41d07397972c04e252bcd2822
Verdant31 <joao.pedro3110@hotmail.com>
2022-10-11 17:28:10 -0300

    Changing the circles component to a triangle component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,25 +1,32 @@
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
-import { BackgroundCircles } from './BackgroundCircles'
 
 export const Hero: React.FC = () => {
-  const [text, count] = useTypewriter({
+  const [text] = useTypewriter({
     words: [
       'Olá, eu sou o João',
       'Eu sou um desenvolvedor React',
-      'Sou um baita fã de joguinhos online',
+      'Sou um fã de joguinhos online',
     ],
     loop: true,
     delaySpeed: 2000,
   })
 
   return (
-    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
-      <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+    <div className="h-screen flex flex-col items-center justify-center text-center overflow-hidden">
+      <div className="relative flex ">
+        <div className="animate-pulse">
+          <img
+            className="w-[500px] h-[430px] opacity-40"
+            src="triangles.png"
+            alt="Imagem de um triangulo"
+          />
+        </div>
+        <h1 className="text-gray-50 w-full absolute top-[50%] left-[50%] -translate-y-1/2	-translate-x-1/2 text-4xl ">
+          <span>{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+      </div>
     </div>
   )
 }
