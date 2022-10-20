865c8104149ac0b72119c06d868e0283fe0fc961
Verdant31 <joao.pedro3110@hotmail.com>
2022-10-20 17:05:16 -0300

    tryng to fix API url problem pt1

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,10 +1,13 @@
+import axios from "axios";
 import { Data } from "../@types/types";
 import { api } from "../services/api";
 
 export const useData = async (): Promise<Data> => {
-  const about = await api.get("/api/about").then((res) => res.data);
-  const projects = await api.get("/api/projects").then((res) => res.data);
-  const experiences = await api.get("/api/experience").then((res) => res.data);
-  const skills = await api.get("/api/skill").then((res) => res.data);
+  const about = await axios.get("/api/about").then((res) => res.data);
+  const projects = await axios.get("/api/projects").then((res) => res.data);
+  const experiences = await axios
+    .get("/api/experience")
+    .then((res) => res.data);
+  const skills = await axios.get("/api/skill").then((res) => res.data);
   return { projects, about, experiences, skills };
 };
