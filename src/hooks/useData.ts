f4bc8f47f449eff023e353ba025c2ebd5f020b73
Verdant31 <joao.pedro3110@hotmail.com>
2022-10-20 16:57:35 -0300

    changing api url

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -3,16 +3,16 @@ import { Data } from "../@types/types";
 
 export const useData = async (): Promise<Data> => {
   const about = await axios
-    .get("http://localhost:3000/api/about")
+    .get(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/about`)
     .then((res) => res.data);
   const projects = await axios
-    .get("http://localhost:3000/api/projects")
+    .get(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/projects`)
     .then((res) => res.data);
   const experiences = await axios
-    .get("http://localhost:3000/api/experience")
+    .get(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/experience`)
     .then((res) => res.data);
   const skills = await axios
-    .get("http://localhost:3000/api/skill")
+    .get(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/skill`)
     .then((res) => res.data);
   return { projects, about, experiences, skills };
 };
