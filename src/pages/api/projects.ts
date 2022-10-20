8a8ba3807a9503c7b16b401f13ba11ccb1a3c677
Verdant31 <joao.pedro3110@hotmail.com>
2022-10-20 17:57:05 -0300

    WIP

--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -9,7 +9,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         "Projeto também do programa Ignite da Rocketseat. O CoffeeDelivery foi criado para ser um e-commerce de café, podendo selecionar produtos para adicionar ao carrinho e realizar checkout. O Projeto foi feito em React e inicializado com a ferramenta ViteJS, além do StyledComponents para estilização.",
       descriptionEn:
         "Project also from Rocketseat's Ignite program. CoffeeDelivery was developed to be a coffee e-commerce, allowing users to buy products  by adding them to a cart and checkout. The Project was made in React and initialized with the ViteJS tool, in addition to StyledComponents for styling.",
-      mockup: `${process.env.NEXT_PUBLIC_VERCEL_URL}/coffe-mockup.png`,
+      mockup: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/coffe-mockup.png`,
     },
     {
       title: "TODO List - Ignite",
@@ -17,7 +17,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         "Este projeto foi um desafio da trilha de ReactJS do programa de aceleração para devs, o Ignite da Rocketseat. O Projeto foi feito em React e inicializado com a ferramenta ViteJS, além do CSS modules para estilização.",
       descriptionEn:
         "This project was a challenge from the ReactJS track of the accelerator program for devs, Rocketseat's Ignite. The Project was made in React and initialized with the ViteJS tool, in addition to CSS modules for styling.",
-      mockup: `${process.env.NEXT_PUBLIC_VERCEL_URL}/todo-mockup.png`,
+      mockup: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/todo-mockup.png`,
     },
   ];
   res.status(200).json(projects);
--- src/pages/api/skill.ts
+++ src/pages/api/skill.ts
@@ -4,82 +4,82 @@ import { Skill } from "../../@types/types";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const skills: Skill[] = [
     {
-      logo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/css.svg`,
+      logo: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/css.svg`,
       name: "CSS3",
       proficiency: 90,
     },
     {
-      logo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/firebase.svg`,
+      logo: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/firebase.svg`,
       name: "Firebase",
       proficiency: 80,
     },
     {
-      logo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/google.svg`,
+      logo: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/google.svg`,
       name: "GCP",
       proficiency: 60,
     },
     {
-      logo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/html.svg`,
+      logo: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/html.svg`,
       name: "HTML5",
       proficiency: 100,
     },
     {
-      logo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/heroku.svg`,
+      logo: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/heroku.svg`,
       name: "Heroku",
       proficiency: 50,
     },
     {
-      logo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/javascript.svg`,
+      logo: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/javascript.svg`,
       name: "Javascript",
       proficiency: 80,
     },
     {
-      logo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/mongo.svg`,
+      logo: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/mongo.svg`,
       name: "MongoDB",
       proficiency: 70,
     },
     {
-      logo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/nextjs.svg`,
+      logo: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/nextjs.svg`,
       name: "NextJS",
       proficiency: 80,
     },
     {
-      logo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/nodejs.svg`,
+      logo: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/nodejs.svg`,
       name: "NodeJS",
       proficiency: 80,
     },
     {
-      logo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/postgre.svg`,
+      logo: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/postgre.svg`,
       name: "PostgreSQL",
       proficiency: 80,
     },
     {
-      logo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/prisma.svg`,
+      logo: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/prisma.svg`,
       name: "Prisma",
       proficiency: 90,
     },
     {
-      logo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/react.svg`,
+      logo: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/react.svg`,
       name: "React",
       proficiency: 100,
     },
     {
-      logo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/styled.svg`,
+      logo: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/styled.svg`,
       name: "Styled Components",
       proficiency: 80,
     },
     {
-      logo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/tailwind.svg`,
+      logo: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/tailwind.svg`,
       name: "Tailwind CSS",
       proficiency: 90,
     },
     {
-      logo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/typescript.svg`,
+      logo: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/typescript.svg`,
       name: "Typescript",
       proficiency: 80,
     },
     {
-      logo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/vite.svg`,
+      logo: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/vite.svg`,
       name: "ViteJS",
       proficiency: 100,
     },
