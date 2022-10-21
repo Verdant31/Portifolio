59b3e92f6d02a58e495a98764ae5b76698e1ea4e
Verdant31 <joao.pedro3110@hotmail.com>
2022-10-20 22:21:46 -0300

    wip

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <link rel="preconnect" href="http://fonts.googleapis.com" />
+          <link rel="preconnect" href="http://fonts.gstatic.com" />
+          <link
+            href="http://fonts.googleapis.com/css2?family=Anton&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
